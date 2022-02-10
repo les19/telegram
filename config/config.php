1455b7633d8ad8e41ac3eabb53df7f2d21a576c8
1ex_19 <lex.19.mov@gmail.com>
2022-02-10 11:15:10 +0200

    chore:updated config comments

--- config/config.php
+++ config/config.php
@@ -1,10 +1,41 @@
 <?php
 
 return [
-  "token" => env('TELEGRAM_TOKEN', null),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Your BOT token from telegram bot father
+  |--------------------------------------------------------------------------
+  */
+
+  "token" => env('TELEGRAM_TOKEd', null),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Base url telegram api
+  |--------------------------------------------------------------------------
+  */
+
   "base" => "https://api.telegram.org/bot",
+
+  /*
+  |--------------------------------------------------------------------------
+  | Base reception class. If nothing is passed to the Telegram::hook method, then this class will be used
+  |--------------------------------------------------------------------------
+  */
+
   "reception" => App\Telegram\Receptions\Reception::class,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Other service params.
+  |--------------------------------------------------------------------------
+  */
+
   "file_base" => "https://api.telegram.org/file/bot",
+
   "file_url" => "https://api.telegram.org/file/bot" . env('TELEGRAM_TOKEN', null),
-  "url" => "https://api.telegram.org/bot" . env('TELEGRAM_TOKEN', null)
+
+  "url" => "https://api.telegram.org/bot" . env('TELEGRAM_TOKEd', null),
+
 ];
